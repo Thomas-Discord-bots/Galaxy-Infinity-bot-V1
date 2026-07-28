c67b6f0f95accad70a20d1b49b90a2eec9c4b15c
ItzRealThomas <thomaslealeikhaugen@gmail.com>
2026-07-28 21:24:26 +0200

    Update bot status message with new features

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
+        state: "!!🚨NEW UPDATE🚨!!: Added music system from YT, Spotify, Deezer and much more!!, Also changed bot name to 'Galaxy Infinity bot V1'",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
