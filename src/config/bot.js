7222e457892bc1f32193dcaf0619fa9d01d57279
ItzRealThomas <thomaslealeikhaugen@gmail.com>
2026-07-31 16:19:56 +0200

    Update bot status message in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "!!🚨NEW UPDATE🚨!!: Added music system from YT, Spotify, Deezer and much more!!, Also changed bot name to 'Galaxy Infinity bot V1'",     // this is what people actually see
+        state: "Im Currently disabled by Thomas. He also disabled discord so DM'ing him is useluss. Have a nice life!",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
